3950150769956b0a1f8d714b7f8c580d21cf1320
CliveC99 <cliveconnell@gmail.com>
2023-08-17 11:53:00 +0000

    Implement delete for the CRUD functionality of the posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import view, PostDetail, CreatePost, EditPost
+from .views import view, PostDetail, CreatePost, EditPost, DeletePost
 
 urlpatterns = [
 #   path('', views.index, name='home'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('car/<int:pk>', PostDetail.as_view(), name='post-details'),
     path('create_post/', CreatePost.as_view(), name='create_post'),
     path('car/edit/<int:pk>', EditPost.as_view(), name='edit_post'),
+    path('car/<int:pk>/delete', DeletePost.as_view(), name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import CreatePostForm
+from django.urls import reverse_lazy
 
 #   def index(request):
 #    return render(request, 'index.html', {})
@@ -27,4 +28,8 @@ class EditPost(UpdateView):
     model = Post
     form_class = CreatePostForm
     template_name = 'edit_post.html'
-    
+
+class DeletePost(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
