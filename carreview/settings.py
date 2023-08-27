5ebbde4b267bb161d37366293d7ec1417020e388
CliveC99 <cliveconnell@gmail.com>
2023-08-27 14:24:49 +0000

    Final deployment

--- carreview/settings.py
+++ carreview/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-clivec99-carrating-cczlwbyn5f6.ws-eu104.gitpod.io', 'car-review-21ed92bc74a0.herokuapp.com']
 
